30e3183312db8ea8523a4a86fc0fe0bc3ffce239
Girin Ilya <39518953+Choos37ricK@users.noreply.github.com>
2019-12-14 13:24:46 +0300

    project finally'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.bson.BsonDocument;
 import org.bson.Document;
+
+import org.apache.commons.csv.CSVParser;
+
+import java.io.*;
+import java.util.*;
+import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,21 +22,48 @@ public class Main {
 
         mongoCollection.drop();
 
-        //import csv
+        File file = new File("mongo.csv");
+        try {
+            Reader reader = new BufferedReader(new FileReader(file));
+            CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT.withDelimiter(','));
+            Iterator<CSVRecord> iterator = parser.iterator();
+
+            Map<String, Object> map;
+
+            int id = 1;
+            while (iterator.hasNext()) {
+                CSVRecord next = iterator.next();
+                map = new HashMap<>();
+                map.put("_id", id);
+                map.put("name", next.get(0).trim());
+                map.put("age", Integer.parseInt(next.get(1).trim()));
+
+                ArrayList<String> coursesList = new ArrayList<>(Arrays.asList(next.get(2).trim().split(",")));
+                map.put("courses", coursesList);
+                Document student = new Document(map);
+
+                mongoCollection.insertOne(student);
+                id++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         System.out.println("Total quantity of the students in db: " + mongoCollection.countDocuments());
 
         BsonDocument query = BsonDocument.parse("{age: {$gt: 40}}");
         System.out.println("Students quantity which age > 40 years: " + mongoCollection.countDocuments(query));
 
-        System.out.println("Name of the youngest student: " + mongoCollection
-                .find(BsonDocument.parse("{{name: 1}}"))
+        mongoCollection
+                .find(BsonDocument.parse("{}, {name: 1, _id: 0}"))
                 .sort(BsonDocument.parse("{age: 1}"))
-                .limit(1));
+                .limit(1)
+                .forEach((Consumer<Document>) document -> System.out.println("Name of the youngest student: " + document.get("name")));
 
-        System.out.println("Name of the youngest student: " + mongoCollection
-                .find(BsonDocument.parse("{{courses: 1}}"))
+        mongoCollection
+                .find(BsonDocument.parse("{}, {courses: 1, _id: 0}"))
                 .sort(BsonDocument.parse("{age: -1}"))
-                .limit(1));
+                .limit(1)
+                .forEach((Consumer<Document>) document -> System.out.println("Courses of the oldest student: " + document.get("courses")));
     }
 }
